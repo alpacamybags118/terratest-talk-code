efab2d43e822f293b7b00f32ca2231c867b50c94
jsakyle@gmail.com <jsakyle@gmail.com>
2020-09-18 00:20:02 -0400

    fixed state issues with parallel run of sqs tests

--- complex-tests-aws/test/sqs/sqs_test.go
+++ complex-tests-aws/test/sqs/sqs_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"testing"
+	"time"
 
 	"github.com/TwinProduction/go-color"
 	"github.com/stretchr/testify/assert"
@@ -12,63 +13,8 @@ import (
 	"github.com/aws/aws-sdk-go/service/sqs"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 )
-
-func TestSqsModuleEnsureKMSEncryption(t *testing.T) {
-	t.Parallel()
-
-	id := random.UniqueId()
-	queueName := fmt.Sprintf("sqs-queue-%s", id)
-
-	terraformOptions := &terraform.Options{
-		TerraformDir: "../../modules/sqs",
-		Vars: map[string]interface{}{
-			"region":     "us-east-2",
-			"queue_name": queueName,
-		},
-		NoColor: true,
-	}
-	defer terraform.Destroy(t, terraformOptions)
-
-	terraform.InitAndApply(t, terraformOptions)
-
-	// Validate the queue exists and is set up for encryption
-	queueURL := terraform.Output(t, terraformOptions, "sqs_queue_url")
-	kmsKeyID := terraform.Output(t, terraformOptions, "kms_key_id")
-
-	options := session.Options{
-		Config: aws.Config{
-			Region: aws.String("us-east-2"),
-		},
-	}
-	session, err := session.NewSessionWithOptions(options)
-
-	if err != nil {
-		fmt.Println(err)
-		t.Fail()
-	}
-	sqsService := sqs.New(session)
-
-	args := &sqs.GetQueueAttributesInput{
-		AttributeNames: []*string{
-			aws.String("KmsMasterKeyId"),
-		},
-		QueueUrl: aws.String(queueURL),
-	}
-	result, err := sqsService.GetQueueAttributes(args)
-
-	if err != nil {
-		t.Fail()
-	}
-
-	keyID := *result.Attributes["KmsMasterKeyId"]
-
-	fmt.Println(color.Ize(color.Yellow, fmt.Sprintf("Expected KMS Key: %s", kmsKeyID)))
-	fmt.Println(color.Ize(color.Yellow, fmt.Sprintf("Actual KMS Key: %s", keyID)))
-
-	assert.Equal(t, kmsKeyID, keyID)
-
-}
 
 func TestSqsModulePutAndGetMessage(t *testing.T) {
 	t.Parallel()
@@ -76,8 +22,10 @@ func TestSqsModulePutAndGetMessage(t *testing.T) {
 	id := random.UniqueId()
 	queueName := fmt.Sprintf("sqs-queue-%s", id)
 
+	modulePath := test_structure.CopyTerraformFolderToTemp(t, "../../", "modules/sqs")
+
 	terraformOptions := &terraform.Options{
-		TerraformDir: "../../modules/sqs",
+		TerraformDir: modulePath,
 		Vars: map[string]interface{}{
 			"region":     "us-east-2",
 			"queue_name": queueName,
@@ -123,18 +71,84 @@ func TestSqsModulePutAndGetMessage(t *testing.T) {
 		QueueUrl: aws.String(queueURL),
 	}
 
-	result, err := sqsService.ReceiveMessage(receiveArgs)
+	var receivedMessage string = ""
 
-	if err != nil {
-		fmt.Println(color.Ize(color.Red, err.Error()))
-		t.FailNow()
+	for i := 0; i < 10; i++ {
+		result, err := sqsService.ReceiveMessage(receiveArgs)
+
+		if err != nil {
+			fmt.Println(color.Ize(color.Red, err.Error()))
+		}
+
+		if len(result.Messages) > 0 {
+			receivedMessage = *result.Messages[0].Body
+			break
+		}
+
+		time.Sleep(1 * time.Second)
 	}
-
-	receivedMessage := *result.Messages[0].Body
 
 	fmt.Println(color.Ize(color.Yellow, "Expected: test message"))
 	fmt.Println(color.Ize(color.Yellow, fmt.Sprintf("Received: %s", receivedMessage)))
 
 	assert.Equal(t, "test message", receivedMessage)
+
+}
+
+func TestSqsModuleEnsureKMSEncryption(t *testing.T) {
+	t.Parallel()
+
+	id := random.UniqueId()
+	queueName := fmt.Sprintf("sqs-queue-%s", id)
+
+	modulePath := test_structure.CopyTerraformFolderToTemp(t, "../../", "modules/sqs")
+
+	terraformOptions := &terraform.Options{
+		TerraformDir: modulePath,
+		Vars: map[string]interface{}{
+			"region":     "us-east-2",
+			"queue_name": queueName,
+		},
+		NoColor: true,
+	}
+	defer terraform.Destroy(t, terraformOptions)
+
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Validate the queue exists and is set up for encryption
+	queueURL := terraform.Output(t, terraformOptions, "sqs_queue_url")
+	kmsKeyID := terraform.Output(t, terraformOptions, "kms_key_id")
+
+	options := session.Options{
+		Config: aws.Config{
+			Region: aws.String("us-east-2"),
+		},
+	}
+	session, err := session.NewSessionWithOptions(options)
+
+	if err != nil {
+		fmt.Println(err)
+		t.Fail()
+	}
+	sqsService := sqs.New(session)
+
+	args := &sqs.GetQueueAttributesInput{
+		AttributeNames: []*string{
+			aws.String("KmsMasterKeyId"),
+		},
+		QueueUrl: aws.String(queueURL),
+	}
+	result, err := sqsService.GetQueueAttributes(args)
+
+	if err != nil {
+		t.Fail()
+	}
+
+	keyID := *result.Attributes["KmsMasterKeyId"]
+
+	fmt.Println(color.Ize(color.Yellow, fmt.Sprintf("Expected KMS Key: %s", kmsKeyID)))
+	fmt.Println(color.Ize(color.Yellow, fmt.Sprintf("Actual KMS Key: %s", keyID)))
+
+	assert.Equal(t, kmsKeyID, keyID)
 
 }
